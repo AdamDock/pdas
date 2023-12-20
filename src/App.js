5f5d5eea753c3f644e064fbd1fdee96f7a9c4889
Adam Dockstader <39977252+AdamDock@users.noreply.github.com>
2023-12-20 15:35:33 -0700

    completed styling of cards and images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import ProfileCard from './ProfileCard';
+import alexaImage from './images/alexa.png';
+import siriImage from './images/siri.png';
+import cortanaImage from './images/cortana.png';
+import 'bulma/css/bulma.css';
 
 function App() {
+  const siri = ['Siri', "Siri is made by apple and is being phased out", siriImage, "@siri01"];
+  const alexa = ['Alexa', "Alexa was created by Amazon to help buy things", alexaImage, "@alexa99"];
+  const google = ['Cortana', "Cortana is made by microsoft", cortanaImage, "@cortana32"];
+  const profile = [siri, alexa, google];
   return (
     <div>
-      <div>Personal Digital Assistants</div>
+      <section className="hero is-primary">
+        <div className='hero-body'>
+          <p className='title'>Personal Digital Assistants</p>
+        </div>
+      </section>
 
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <div className='container'>
+        <section className='section'>
+          <div className='columns'>
+          {profile.map((profile, index)=>{
+  return <div className='column is-4'><ProfileCard name={profile[0]} intro={profile[1]} image={profile[2]} key={index} handle={profile[3]} /></div>
+})}  
+          </div>
+        </section>
+
+      </div>
+    
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,22 @@
-function ProfileCard() {
-  return <div>Profile Card!!!</div>;
+function ProfileCard({name, intro, image, handle}) {
+  return <>
+  <div className="card">
+    <div className="card-image">
+      <figure className="image is-1by1">
+        <img src={image} alt={name} />
+      </figure>
+    </div>
+    <div className="card-content">
+      <div className="media-content">
+        <p className="title is-4">{name}</p>
+        <p className="title is-6">{intro}</p>
+        <p className="title is-6">{handle}</p>
+      </div>
+    </div>
+      
+  </div>
+  
+  </>
 }
 
 export default ProfileCard;
